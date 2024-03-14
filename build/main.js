8f356414168bee26f23509c9efdfcace9e7fdb36
Artem <bezkorovainy2006@gmail.com>
2024-03-14 18:59:01 +0100

    preparing package

---
+++ build/main.js
@@ -0,0 +1,8 @@
+class App {
+  run() {
+    const name = 'za warudo';
+    console.log(name);
+  }
+}
+const app = new App();
+app.run();
---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+class App {
+    run() {
+        const name = 'za warudo'
+        console.log(name);
+    }
+}
+
+const app = new App();
+app.run();
